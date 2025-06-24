34516a51355ea8dc2034ea64a9d1205b1bf2e22f
Rudra Somaiya <rudrasomaiya0@gmail.com>
2025-06-24 12:05:17 +0530

    Changed startup to work for windows (linux has to edit)

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 cd backend
 # Activate venv if it exists
 if [ -d "venv" ]; then
-    source venv/bin/activate
+    source venv/scripts/activate
 fi
 # Run uvicorn on all interfaces for Docker
 exec uvicorn app:app --host 0.0.0.0 --port 5000
